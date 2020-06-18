0bc9717c00fa6bc66a5e6c9a116c96b948900618
withHappy <hap2.dev@gmail.com>
2020-06-18 12:14:32 +0900

    refactor : update shortener model

--- url_shorten/url_shorten/settings/base.py
+++ url_shorten/url_shorten/settings/base.py
@@ -14,8 +14,8 @@ import os
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-env_file = os.path.dirname(os.path.dirname(BASE_DIR)) + '/.env'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+env_file = os.path.dirname(BASE_DIR) + '/.env'
 environ.Env.read_env(env_file=env_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'shortener',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
