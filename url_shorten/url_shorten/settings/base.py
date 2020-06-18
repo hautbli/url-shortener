c07af1956fd473d4f9c7cd179e3c60885ede5980
withHappy <hap2.dev@gmail.com>
2020-06-18 20:17:32 +0900

    refactor : add anon throttle_classes

--- url_shorten/url_shorten/settings/base.py
+++ url_shorten/url_shorten/settings/base.py
@@ -139,4 +139,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'url_shorten.paginations.listpagination',
+    'PAGE_SIZE': 3,
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute'
+    }
 }
